075860a6b8f05d2d0aada2b273bbbb7e1f68cf9d
JohannR513 <57722211+JohannR513@users.noreply.github.com>
2020-09-15 20:20:33 -0500

    Add files via upload

---
+++ Parcial POO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FachadaUniversidad.Reservas;
+
+namespace Fachada_Universidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reservar fachadaUniversidad = new Reservar();
+            fachadaUniversidad.SaludarCliente();
+        }
+    }
+}
