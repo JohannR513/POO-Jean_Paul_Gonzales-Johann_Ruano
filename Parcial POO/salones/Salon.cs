13ec64266ee2ebe3c1393f15d9c914fb4298752f
JohannR513 <57722211+JohannR513@users.noreply.github.com>
2020-09-15 20:15:45 -0500

    Add files via upload

---
+++ Parcial POO/salones/Salon.cs
@@ -0,0 +1,15 @@
+namespace FachadaUniversidad.salones
+{
+    public class Salon
+    {
+        #region Properties
+        public string Nombre { get; set; }
+        public string OpcionUsuario { get; set; }
+        public float temperaturaSalon { get; set; }
+        public string estadoSalon { get; set; }
+        public bool estadopuertas { get; set; }
+        #endregion Properties
+
+    }
+
+}
